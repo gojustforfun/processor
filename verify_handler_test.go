65c6bb4cd687a533dd704ca0d2461088f44ea4ff
longyue0521 <longyueli0521@gmail.com>
2022-01-21 18:48:17 +0800

    refactor: abstract setup func to make test more readable and maintainable

--- verify_handler_test.go
+++ verify_handler_test.go
@@ -7,16 +7,17 @@ import (
 )
 
 func TestVerifierReceivesInput(t *testing.T) {
+	handler, out := setup()
+	handler.Listen()
+	assert.Equal(t, 1, <-out)
+}
+
+func setup() (handler *VerifyHandler, out chan interface{}) {
 	in := make(chan interface{}, 10)
-	out := make(chan interface{}, 10)
-	verifier := NewVerifier(in, out)
-
-	expected := 1
-	in <- expected
+	out = make(chan interface{}, 10)
+	handler = NewVerifier(in, out)
+	
+	in <- 1
 	close(in)
-
-	verifier.Listen()
-
-	assert.Equal(t, expected, <-out)
-
+	return 
 }
