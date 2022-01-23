dcc9522eccaf2c2f4e7f67d936c95eebe0d34b4d
longyue0521 <longyueli0521@gmail.com>
2022-01-23 16:56:19 +0800

    refactor: rename variable to make code more readable

--- verify_handler.go
+++ verify_handler.go
@@ -1,26 +1,26 @@
 package processor
 
 type VerifyHandler struct {
-	in          chan *Envelope
-	out         chan *Envelope
-	application Verifier
+	input    chan *Envelope
+	output   chan *Envelope
+	verifier Verifier
 }
 
 type Verifier interface {
 	Verify(AddressInput) AddressOutput
 }
 
-func NewVerifierHandler(in, out chan *Envelope, application Verifier) *VerifyHandler {
+func NewVerifierHandler(input, output chan *Envelope, verifier Verifier) *VerifyHandler {
 	return &VerifyHandler{
-		in:          in,
-		out:         out,
-		application: application,
+		input:    input,
+		output:   output,
+		verifier: verifier,
 	}
 }
 
 func (v *VerifyHandler) Handle() {
-	for  envelope := range v.in {
-		envelope.Output = v.application.Verify(envelope.Input)
-		v.out <- envelope
+	for envelope := range v.input {
+		envelope.Output = v.verifier.Verify(envelope.Input)
+		v.output <- envelope
 	}
 }
