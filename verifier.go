b3a640287cdfc04ff6ccce33a0ef06d7dbaad7cd
longyue0521 <longyueli0521@gmail.com>
2022-01-24 16:30:38 +0800

    refactor: refactor Verify method by abstract methods
    abstract buildRequest method
    abstract decodeResponse method
    abstract translateCandidate method

--- verifier.go
+++ verifier.go
@@ -22,6 +22,16 @@ func NewSmartyVerifier(client HTTPClient) *SmartyVerifier {
 
 func (s *SmartyVerifier) Verify(input AddressInput) AddressOutput {
 
+	request := s.buildRequest(input)
+
+	response, _ := s.client.Do(request)
+
+	output := s.decodeResponse(response)
+
+	return s.translateCandidate(output[0])
+}
+
+func (s *SmartyVerifier) buildRequest(input AddressInput) *http.Request {
 	query := url.Values{}
 	query.Set("street", input.Street1)
 	query.Set("city", input.City)
@@ -29,12 +39,17 @@ func (s *SmartyVerifier) Verify(input AddressInput) AddressOutput {
 	query.Set("zipcode", input.ZIPCode)
 
 	request, _ := http.NewRequest(http.MethodGet, "/street-address?"+query.Encode(), nil)
-	response, _ := s.client.Do(request)
+	return request
+}
 
-	var output []Candidate
+func (s *SmartyVerifier) decodeResponse(response *http.Response) (output []Candidate) {
 	json.NewDecoder(response.Body).Decode(&output)
+	return output
+}
+
+func (s *SmartyVerifier) translateCandidate(candidate Candidate) AddressOutput {
 	return AddressOutput{
-		DeliveryLine1: output[0].DeliveryLine1,
+		DeliveryLine1: candidate.DeliveryLine1,
 	}
 }
 
